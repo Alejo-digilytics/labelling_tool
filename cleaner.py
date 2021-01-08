261bfd706c5c69be2d7a4e8f1457c7fcb0b7f647
AlejoLopezAvila <alejo.lopez@digilytics.ai>
2021-01-08 20:38:49 +0000

    add cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -1,5 +1,7 @@
 import re
 import pandas as pd
+import os
+import nltk
 
 SWITCHER = {
     "O": "O",
@@ -26,7 +28,7 @@ SWITCHER = {
 }
 
 
-def cleaner(path_file):
+def cleaner(path_file, output_path):
     df = pd.read_csv(path_file, names=["word", "tag"], sep='\t')
     list_given_entities = df["tag"].unique().tolist()
 
@@ -45,14 +47,43 @@ def cleaner(path_file):
 
     # clean tags
     list_entities = df["tag"].tolist()
-    output = clean_entities(list_entities, SWITCHER)
-    df["tag"] = output
-    df["text"] = df["word"] + " " + df["tag"] + " "
-    text = df["text"].tolist()
-    text = "".join(text)
-    re.sub("\s+", " ", text)
-    return text
+    tagged_text = clean_entities(list_entities, SWITCHER)
 
+    # Add tagged words
+    df["tag"] = tagged_text
+    df["text"] = df["word"] + " " + df["tag"] + " "
+
+    # Join and clean text
+    text = "".join(df["text"].tolist())
+    re.sub("\s+", " ", text)
+
+    # Split text based on real sentences
+    tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+    list_sentences = [sentence for sentence in tokenizer.tokenize(text)]
+
+    # Split based on size
+    list_ = []
+    sentence_size = 18
+    max_size = 35
+    for line in list_sentences:
+        words = line.split(" ")
+        num_words = len(words)
+        # Too long
+        if num_words > 35:
+            divider = num_words//sentence_size
+            for i in range(divider):
+                list_.append(" ".join(words[i*sentence_size:(i+1)*sentence_size]))
+            list_.append(" ".join(words[divider*sentence_size:]))
+        else:
+            list_.append(line)
+    for element in list_:
+        if isinstance(element,list):
+            print("It is  a trap")
+    file = open(output_path, "w+", encoding="UTF-8")
+    file.close()
+    for line in list_:
+        with open(output_path, "a", encoding="UTF-8") as the_file:
+            the_file.write(line + "\n")
 
 def clean_entities(list_entities, SWITCHER):
     last_entity = ""
@@ -88,7 +119,6 @@ def clean_entities(list_entities, SWITCHER):
         position += 1
     return output
 
-
-text = cleaner(path_file='BankStatements (3).tsv')
-print(text)
-
+path_file='BankStatements.tsv'
+FILE_PATH = os.path.join(os.getcwd(), "Data", path_file.split(".tsv")[0] + ".txt")
+cleaner(path_file=path_file, output_path=FILE_PATH)
